ab6c900e023ca89ddde431f8be35e378c1f47c21
Ignatius Odi <ignodi@gmail.com>
2022-10-23 23:36:12 +0700

    MainActivity, VS Player Layouting

--- app/src/main/java/com/cbengineer/onitamaai/MainActivity.kt
+++ app/src/main/java/com/cbengineer/onitamaai/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.cbengineer.onitamaai
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btVsPlayer: Button
+    lateinit var btVsAI: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btVsPlayer = findViewById(R.id.btVsPlayer)
+        btVsAI = findViewById(R.id.btVsAI)
+        
+        btVsPlayer.setOnClickListener {
+            startActivity(Intent(this, PlayerActivity::class.java))
+        }
+        
+        btVsAI.setOnClickListener {
+            Toast.makeText(this, "COMING SOON", Toast.LENGTH_SHORT).show()
+        }
     }
 }
